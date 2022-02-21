973949794714601fb88fe9ead512f88ed4659f19
steveBstorm <steve.lorent@bstorm.be>
2022-02-21 16:37:32 +0100

    Ajoutez des fichiers projet.

---
+++ DemoSecurity/AuthProvider/DemoAuthStateProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace DemoSecurity.AuthProvider
+{
+    public class DemoAuthStateProvider : AuthenticationStateProvider
+    {
+        [Inject]
+        public IJSRuntime js { get; set; }
+        public string token { get; set; }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //await Task.Delay(2000);
+
+            List<Claim> claims = new List<Claim>();
+
+
+            //claims.Add(new Claim(ClaimTypes.Name, "Arthur"));
+            //claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+
+            //string token = await js.InvokeAsync<string>("localStorage.getItem", "currentToken");
+
+            ClaimsIdentity currentUser;
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                currentUser = new ClaimsIdentity();
+                return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(currentUser)));
+            }
+            
+            JwtSecurityToken jwt = new JwtSecurityToken(token);
+
+            foreach (Claim item in jwt.Claims)
+            {
+                claims.Add(item);
+            }
+
+            currentUser = new ClaimsIdentity(claims, "demoAuth");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(currentUser)));
+        }
+
+        public void NotifyUserConnected()
+        {
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
---
+++ DemoSecurity/Pages/Auth/Login.cs
@@ -0,0 +1,48 @@
+﻿using DemoSecurity.AuthProvider;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.Text;
+using System.Text.Json;
+namespace DemoSecurity.Pages.Auth
+{
+    public partial class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        HttpClient _client { get; set; }
+
+        [Inject]
+        public IJSRuntime js{ get; set; }
+
+        [Inject]
+        public IServiceProvider _serviceProvider { get; set; }
+
+        
+
+        public void SubmitLogin()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://localhost:44338/api/");
+
+            string jsonContent = JsonSerializer.Serialize(new { Email = Email, Password = Password });
+            HttpContent content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            using(HttpResponseMessage response = _client.PostAsync("auth", content).Result)
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = response.Content.ReadAsStringAsync().Result;
+
+                    js.InvokeVoidAsync("localStorage.setItem", "currentToken", jsonResponse);
+
+                    // MyProperty.NotifyUserConnected();
+                    ((DemoAuthStateProvider)_serviceProvider.GetService<AuthenticationStateProvider>()).token = jsonResponse;
+                     ((DemoAuthStateProvider)_serviceProvider.GetService<AuthenticationStateProvider>()).NotifyUserConnected();
+                }
+            }
+        }
+    }
+}
